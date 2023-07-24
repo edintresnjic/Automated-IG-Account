a9cfec7dc91e042d57ea4072c53ead01525826d9
Edin <123777724+edintresnjic@users.noreply.github.com>
2023-07-24 14:35:37 +0200

    Finished Project (#8)
    * Text centered and with an outline
    
    * Random quote
    
    * Updated the quotes api to a better one, and added audio to video clip

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from PIL import ImageFont, ImageDraw, Image  
 import cv2  
+import requests
 import numpy as np 
+import moviepy.editor as mpe
+from moviepy.editor import *
+import os
 
 random_num = np.random.randint(1, 7)
+random_music = np.random.randint(1, 6)
 cap = cv2.VideoCapture(f'template_videos/{random_num}.mp4')
 
 frame_width = int(cap.get(3))
@@ -12,29 +17,75 @@ size = (frame_width, frame_height)
 fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
 result = cv2.VideoWriter(f'exported_videos/exported_video{random_num}.avi', fourcc, 30, size)
 
-font_size = 30
+font_size = 43
+
+QUOTE_DATA = {
+    "limit": 1,
+    "maxLength": 30,
+    "minLength": 10,
+}
+
+def get_motivational_quote(max_length=30):
+    try:
+        response = requests.get("https://api.quotable.io/quotes/random", QUOTE_DATA)
+        data = response.json()
+        print(data)
+        quote = data[0]["content"]
+
+        return quote
+            
+    except requests.exceptions.RequestException as e:
+        return f"Error: {e}"
+    except (KeyError, IndexError):
+        return "Error: Unable to fetch quote"
+
+# Example usage:
+text = get_motivational_quote(max_length=30)
+    
 
 while(True):
-      
+    
+    ## MAIN FRAME HANDLING
     # Capture frames in the video
     ret, frame = cap.read()
-  
-    # describe the type of font
-    # to be used.
+    if ret == None:
+        break
     font = ImageFont.truetype("THEBOLDFONT.ttf", font_size)
     
-    # Convert the image to RGB (OpenCV uses BGR)  
-    cv2_im_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
+    # Convert the image to RGB (OpenCV uses BGR) to use for pillow:
+    try:
+        cv2_im_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
+    except:
+        print("Reached end")
+        break
    
     # Pass the image to PIL  
     pil_im = Image.fromarray(cv2_im_rgb) 
-
     draw = ImageDraw.Draw(pil_im)
+
+    # MAIN TEXT
     W = cap.get(3)
     H = cap.get(4)
 
-    draw.text(((W-font_size)/2,(H-font_size)/2), "One day, or day one.", font=font)
+    text_width, text_height = draw.textbbox((0, 0), text, font=font)[2:]
+    text_x = (W - text_width) // 2
+    text_y = (H - text_height) // 2
     
+    # OUTLINE
+    outline_color = (0, 0, 0)
+
+    # Outline created with multiple text with slight offsets
+    outline_offsets = [
+        (-3, -3), (-3, 3), (3, -3), (3, 3),
+        (-2, -2), (-2, 2), (2, -2), (2, 2),
+        (-1, -1), (-1, 1), (1, -1), (1, 1)
+    ]
+    for offset_x, offset_y in outline_offsets:
+        draw.text((text_x + offset_x, text_y + offset_y), text, fill=outline_color, font=font)
+
+    draw.text((text_x,text_y),  text, fill=(255, 255, 255), font=font)
+    
+    ## MAIN VIDEO HANDLING
     cv2_im_processed = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)  
 
     cv2.imshow('video', cv2_im_processed)
@@ -50,3 +101,14 @@ cap.release()
 result.release()
 # close all windows
 cv2.destroyAllWindows()
+
+# Add Music
+clip = mpe.VideoFileClip(f'exported_videos/exported_video{random_num}.avi')
+sped_up_clip = clip.fx(vfx.speedx, 2)
+audio_bg = mpe.AudioFileClip(f'music/{random_music}.MP3')
+final_clip = sped_up_clip.set_audio(audio_bg)
+final_clip.write_videofile(f'finished_videos/exported_video{random_num}.mp4')
+
+# Remove exported video
+os.remove(f'exported_videos/exported_video{random_num}.avi')
+
